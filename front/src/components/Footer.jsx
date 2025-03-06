76d2afe07c675beafd8d869ae17eff627207afcc
Adrian <adrims91@gmail.com>
2025-03-06 19:08:13 +0100

    añadido menu

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
     return (
         <>
-            <footer className="d-flex flex-column justify-content-center mt-4 pb-4">
+            <footer className="d-flex flex-column justify-content-center mt-5 pb-4">
                 <h2 className="text-center mb-3 text-decoration-underline">Síguenos en Nuestras Redes Sociales!</h2>
                 <div className="d-flex justify-content-evenly">
                 <div>
--- front/src/components/Menu.jsx
+++ front/src/components/Menu.jsx
@@ -1,32 +1,153 @@
-import { Container, ListGroup } from "react-bootstrap";
-
+import { Col, Container, Row } from "react-bootstrap";
+import { PiCoffeeLight } from "react-icons/pi";
+import { VscCoffee } from "react-icons/vsc";
+import { GiTeapot } from "react-icons/gi";
+import { GiCakeSlice } from "react-icons/gi";
+import { GiSandwich } from "react-icons/gi";
 
 const Menu = () => {
+  
 
-    const bebidas = [
-        {id: 1, nombre: 'Coca-Cola', precio: 2.5},
-        {id: 2, nombre: 'Fanta de Naranja', precio: 2.5}
-    ]
-
-    return (
-        <>
-            <Container fluid className="w-50">
-                <h1 className="text-center">Menú</h1>
-                <ListGroup >
-                {bebidas.map(bebida => (
-                    <ListGroup.Item className="d-flex justify-content-around" key={bebida.id}>
-                        <div>
-                        {bebida.nombre}
-                        </div> 
-                        <div>
-                        {bebida.precio}€
-                        </div>
-                    </ListGroup.Item>
-                    ))}
-                </ListGroup>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Container fluid className="w-75">
+        <Row xs={2} className="mt-5">
+          <Col>
+            <h2><label className="cafelatte">CafeLatte</label> <br/><strong className="menu fw-bold"> MENU</strong></h2>
+          </Col>
+          <Col className="d-flex justify-content-center">
+            <PiCoffeeLight size={125} opacity={0.8} />
+          </Col>
+          <Col className="text-decoration-underline"> 
+            <h3 className="d-flex align-items-center"><VscCoffee className="me-2" size={35} />CAFÉ</h3>
+          </Col>
+        </Row>
+        <div style={{height: '2px'}} className="bg-dark w-50"></div>
+        <Row xs={1} md={2} className="mt-5">
+          <Col className="d-flex justify-content-between">
+            <h5>Espresso</h5>
+            <p>3€</p>
+          </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Americano</h5>
+                <p>3.5€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Latte</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Cappuccino</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Macchiato</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Mocha</h5>
+                <p>4€</p>
+            </Col>
+          </Row>
+        <Row xs={1} md={2} className="mt-5">
+            <Col className="text-decoration-underline">
+                <h3 className="d-flex align-items-center"><GiTeapot className="me-2" size={35} />TÉ</h3>
+            </Col>
+        </Row>
+        <div style={{height: '2px'}} className="bg-dark w-50"></div>
+        <Row xs={1} md={2} className="mt-5">
+            <Col className="d-flex justify-content-between">
+                <h5>Green Tea</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Black Tea</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Chai Tea</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Herbal Tea</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Matcha</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Yerba Mate</h5>
+                <p>4€</p>
+            </Col>
+        </Row>
+        <Row xs={1} md={2} className="mt-5">
+            <Col className="text-decoration-underline">
+                <h3 className="d-flex align-items-center"><GiCakeSlice className="me-2" size={35} />DULCES</h3>
+            </Col>
+        </Row>
+        <div style={{height: '2px'}} className="bg-dark w-50"></div>
+        <Row xs={1} md={2} className="mt-5">
+            <Col className="d-flex justify-content-between">
+                <h5>Croissant</h5>
+                <p>2€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Chocolate Croissant</h5>
+                <p>2.5€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Almond Croissant</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Cinnamon Roll</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Blueberry Muffin</h5>
+                <p>3€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Chocolate Muffin</h5>
+                <p>3€</p>
+            </Col>
+            </Row>
+        <Row xs={1} md={2} className="mt-5">
+            <Col className="text-decoration-underline">
+                <h3 className="d-flex align-items-center"><GiSandwich className="me-2" size={35} />SALADOS</h3>
+            </Col>
+        </Row>
+        <div style={{height: '2px'}} className="bg-dark w-50"></div>
+        <Row xs={1} md={2} className="mt-5">
+            <Col className="d-flex justify-content-between">
+                <h5>Ham & Cheese Croissant</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Spinach & Feta Croissant</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Tomato & Mozzarella Croissant</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Ham & Cheese Sandwich</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Chicken & Avocado Sandwich</h5>
+                <p>4€</p>
+            </Col>
+            <Col className="d-flex justify-content-between">
+                <h5>Salmon & Cream Cheese Sandwich</h5>
+                <p>4€</p>
+            </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default Menu;
